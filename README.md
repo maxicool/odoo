4f331fad20d8d46347959aaae968fec6e4dc53bc
Justin Zhu <justin@go2newera.com>
2017-05-06 23:49:18 +0800

    Create a session model, We check if we can use upgrade app, to update models

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
-# odoo
-odoo apps 
-it is just for tutorial 
+odoo
+make an own apps open academy
+
+it is a tutorial
+
+some issues:
+    1) sometimes i have stop whole odoo server to reinstall this apps
+    2) upgrade has no effect after having changes in models.py, only a reinstall works 
--- models/models.py
+++ models/models.py
@@ -11,6 +11,28 @@ class Course(models.Model):
     #description = fields.Text(string="what is up"
     description = fields.Text()
 
+'''
+Create a session model
+For the module Open Academy, we consider a model for sessions: a session is an
+occurrence of a course taught at a given time for a given audience.
+
+Create a model for sessions. A session has a name, a start date, a duration and
+a number of seats. Add an action and a menu item to display them. Make the new
+model visible via a menu item.
+
+Create the class Session in openacademy/models/models.py.
+Add access to the session object in openacademy/view/openacademy.xml.
+'''
+
+# We check if we can use upgrade app, to update models
+class Session(models.Model):
+    _name = 'openacademy.session'
+
+    name = fields.Char(required=True)
+    start_date = fields.Date()
+    duration = fields.Float(digits=(6, 2), help="Duration in days")
+    seats = fields.Integer(string="Number of seats")
+
 # class openacademy(models.Model):
 #     _name = 'openacademy.openacademy'
 
