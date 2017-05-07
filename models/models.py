7e219ef9c6bc260e123ba9864c4f79ff6159bac7
Justin Zhu <justin@go2newera.com>
2017-05-07 21:28:08 +0800

    Onchange and warning, but warning doesnt work in myway

--- models/models.py
+++ models/models.py
@@ -83,3 +83,20 @@ class Session(models.Model):
             else:
                 # the percentage of
                 r.taken_seats = 100.0 * len(r.attendee_ids) /r.seats
+
+    @api.onchange('seats', 'attendee_ids')
+    def _verify_valid_seats(self):
+        if self.seats < 0:
+            return {
+                'warning': {
+                    'title': "Incorrect 'seats' value",
+                    'message': "The number of available seats may not be negative",
+                },
+            }
+        if self.seats < len(self.attendee_ids):
+            return {
+                'warning': {
+                    'title': "Too many attendees",
+                    'message': "Increase seats or remove excess attendees",
+                },
+            }
